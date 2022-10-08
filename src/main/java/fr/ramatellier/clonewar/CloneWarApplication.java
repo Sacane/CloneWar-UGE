a86d2df3e0516182255b33827b2795425645c743
Sacane <johan.ramaroson@gmail.com>
2022-10-08 22:40:49 +0200

    Init spring basics project's files

---
+++ src/main/java/fr/ramatellier/clonewar/CloneWarApplication.java
@@ -0,0 +1,13 @@
+package fr.ramatellier.clonewar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneWarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ramatellier/clonewar/CloneWarApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ramatellier.clonewar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
