e7560238133b9bbb1c87acf78a3d79663a38b364
Sacane <johan.ramaroson@gmail.com>
2022-10-17 17:37:59 +0200

    add persistence's annotations for instructions

--- src/main/java/fr/ramatellier/clonewar/instruction/Instruction.java
+++ src/main/java/fr/ramatellier/clonewar/instruction/Instruction.java
@@ -1,28 +1,33 @@
 package fr.ramatellier.clonewar.instruction;
 
 import fr.ramatellier.clonewar.util.Hasher;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.UUID;
 
 @Entity
+@Table(name="instruction")
 public class Instruction {
+
+    private static final long serialUUID = 2233498293L;
 
     @Id
     @GeneratedValue
     private UUID id;
 
+    @Column(name = "filename")
     private String filename;
     @NotBlank
+    @Column(name = "startLine")
     private int lineNumberStart;
 
     @NotBlank
+    @Column(name = "content")
     private String content; //Each piece of instruction is separate with \n
 
     @NotBlank
+    @Column(name = "hash_value")
     private long hash;
 
     public Instruction(){}
