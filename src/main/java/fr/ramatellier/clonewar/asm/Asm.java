d6fe9d01940d50e3dd4ecdbf023806c946281895
qtdrake <quentintellier01@yahoo.com>
2022-10-18 18:47:01 +0200

    Modification for abstraction of the bytecode

--- src/main/java/fr/ramatellier/clonewar/asm/Asm.java
+++ src/main/java/fr/ramatellier/clonewar/asm/Asm.java
@@ -14,7 +14,8 @@ import java.util.Arrays;
 public class Asm {
     public static void main(String[] args) throws IOException {
         var builder = new InstructionBuilder();
-        var finder = ModuleFinder.of(Path.of("TestJar.jar"));
+        var file_name = "TestJar.jar";
+        var finder = ModuleFinder.of(Path.of(file_name));
         var moduleReference = finder.findAll().stream().findFirst().orElseThrow();
         try(var reader = moduleReference.open()) {
             for(var filename: (Iterable<String>) reader.list()::iterator) {
@@ -39,7 +40,7 @@ public class Asm {
                         }
                         @Override
                         public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-                            System.err.println("class " + modifier(access) + " ---> NEW INSTRUCTION CLASS " + name);
+                            System.err.println("class " + modifier(access)); // " ---> NEW INSTRUCTION CLASS " + name
                         }
 
                         @Override
@@ -50,27 +51,15 @@ public class Asm {
 
                         @Override
                         public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
-                            System.err.println("  field " + modifier(access) + " " + ClassDesc.ofDescriptor(descriptor).displayName() + " " + signature);
+                            System.err.println("  field " + modifier(access) + " " + ClassDesc.ofDescriptor(descriptor).displayName());
                             return null;
                         }
 
                         @Override
                         public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
-                            System.err.println("  method " + modifier(access) + " " + MethodTypeDesc.ofDescriptor(descriptor).displayDescriptor() + " ---> NEW INSTRUCTION method " + (name.equals("<init>") ? "constructor" : name));
+                            System.err.println("  method " + modifier(access) + " " + MethodTypeDesc.ofDescriptor(descriptor).displayDescriptor()); // " ---> NEW INSTRUCTION method " + (name.equals("<init>") ? "constructor" : name)
 
                             return new MethodVisitor(Opcodes.ASM9) {
-
-                                private String abstractInstr(int opcode){
-                                    return switch(opcode){
-                                        case    Opcodes.ARETURN,
-                                                Opcodes.FRETURN,
-                                                Opcodes.DRETURN,
-                                                Opcodes.IRETURN,
-                                                Opcodes.LRETURN,
-                                                Opcodes.RETURN -> "Return";
-                                        default -> String.valueOf(opcode);
-                                    };
-                                }
 
                                 @Override
                                 public void visitLineNumber(int line, Label start) {
@@ -82,7 +71,7 @@ public class Asm {
                                  * Les 3 fonctions private suivantes servent à abstraire des instructions génériques, exemple :
                                  * Tout types de return sont des Return
                                  */
-                                private boolean isLoadInstr(int opcode){
+                                private boolean isLoadInstr(int opcode) {
                                     return switch (opcode){
                                         case    Opcodes.ALOAD,
                                                 Opcodes.LLOAD,
@@ -92,7 +81,22 @@ public class Asm {
                                         default -> false;
                                     };
                                 }
-                                private boolean isReturnInstr(int opcode){
+
+                                private boolean isALoadInstr(int opcode) {
+                                    return switch(opcode) {
+                                        case Opcodes.IALOAD,
+                                                Opcodes.LALOAD,
+                                                Opcodes.FALOAD,
+                                                Opcodes.DALOAD,
+                                                Opcodes.AALOAD,
+                                                Opcodes.BALOAD,
+                                                Opcodes.CALOAD,
+                                                Opcodes.SALOAD-> true;
+                                        default -> false;
+                                    };
+                                }
+
+                                private boolean isReturnInstr(int opcode) {
                                     return switch(opcode){
                                         case    Opcodes.ARETURN,
                                                 Opcodes.FRETURN,
@@ -104,7 +108,7 @@ public class Asm {
                                     };
                                 }
 
-                                private boolean isStoreInstr(int opcode){
+                                private boolean isStoreInstr(int opcode) {
                                     return switch(opcode){
                                         case Opcodes.ASTORE,
                                                 Opcodes.DSTORE,
@@ -114,20 +118,34 @@ public class Asm {
                                         default -> false;
                                     };
                                 }
+
+                                private boolean isAStoreInstr(int opcode) {
+                                    return switch(opcode) {
+                                        case Opcodes.IASTORE,
+                                                Opcodes.LASTORE,
+                                                Opcodes.FASTORE,
+                                                Opcodes.DASTORE,
+                                                Opcodes.AASTORE,
+                                                Opcodes.BASTORE,
+                                                Opcodes.CASTORE,
+                                                Opcodes.SASTORE-> true;
+                                        default -> false;
+                                    };
+                                }
                                 @Override
                                 public void visitInsn(int opcode) {
-                                    var code = (isStoreInstr(opcode)) ? "Store" : (isLoadInstr(opcode)) ? "Load" : (isReturnInstr(opcode)) ? "Return" : String.valueOf(opcode);
-                                    System.err.println("    opcode " + code);
+                                    var code = (isStoreInstr(opcode)) ? "Store" : (isAStoreInstr(opcode)) ? "AStore" : (isLoadInstr(opcode)) ? "Load" : (isALoadInstr(opcode)) ? "ALoad" : (isReturnInstr(opcode)) ? "Return" : String.valueOf(opcode);
+                                    System.err.println("opcode " + code);
                                 }
 
                                 @Override
                                 public void visitIntInsn(int opcode, int operand) {
-                                    System.err.println("    opcode : " + opcode + " operand : " + operand);
+                                    System.err.println("opcode : " + opcode + " operand : " + operand);
                                 }
 
                                 @Override
                                 public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
-                                    System.err.println("    opcode field " + opcode);
+                                    System.err.println("opcode field " + opcode);
                                 }
 
                                 @Override
@@ -141,25 +159,23 @@ public class Asm {
                                         }
                                         default -> "";
                                     };
-                                    System.err.println("    opcode " + opcode + " " + s);
+                                    System.err.println("opcode " + opcode + " " + s);
                                 }
 
                                 @Override
                                 public void visitVarInsn(int opcode, int varIndex) {
-                                    System.err.println("    opcode " + opcode + " " + varIndex);
+                                    System.err.println("opcode " + opcode + " " + varIndex);
                                 }
 
                                 @Override
                                 public void visitTypeInsn(int opcode, String type) {
-                                    System.err.println("    opcode " + opcode + " type"); //TODO enlever type
+                                    System.err.println("opcode " + opcode);
                                 }
 
                                 @Override
                                 public void visitEnd() {
-                                    System.err.println("    end");
+                                    System.err.println("end");
                                 }
-
-                                // + the other visit methods to get all the opcodes
                             };
                         }
                     }, 0);
