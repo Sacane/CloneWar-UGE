6158c18f7db3f7141488cb87b9cff99f71f69c66
Sacane <johan.ramaroson@gmail.com>
2022-10-20 01:10:04 +0200

    add controller and implement its first method

---
+++ src/main/java/fr/ramatellier/clonewar/controller/ArtifactController.java
@@ -0,0 +1,33 @@
+package fr.ramatellier.clonewar.controller;
+
+import fr.ramatellier.clonewar.dto.ArtifactSaveDTO;
+import fr.ramatellier.clonewar.persistence.model.Artifact;
+import fr.ramatellier.clonewar.persistence.repository.ArtifactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.List;
+
+@RestController
+public class ArtifactController {
+    @Autowired
+    private ArtifactRepository service;
+
+
+    @PostMapping(path = "/api/artifact/save")
+    public Mono<Artifact> save(@RequestBody ArtifactSaveDTO artifact){
+        var entity = new Artifact(artifact.name(), artifact.url(), artifact.date());
+        service.save(entity);
+        return Mono.just(entity);
+    }
+    @GetMapping(path= "/api/test")
+    public Flux<String> send(){
+        return Flux.fromIterable(List.of("String1", "String2", "String3")).delayElements(Duration.ofMillis(150));
+    }
+}
