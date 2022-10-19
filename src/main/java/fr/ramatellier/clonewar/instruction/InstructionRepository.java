d0728afa58a4f023d87e36f2e92d1b00a0d9694b
Sacane <johan.ramaroson@gmail.com>
2022-10-20 01:13:59 +0200

    turn instructionRepository from reactive to crud repository

--- src/main/java/fr/ramatellier/clonewar/instruction/InstructionRepository.java
+++ src/main/java/fr/ramatellier/clonewar/instruction/InstructionRepository.java
@@ -1,5 +1,8 @@
 package fr.ramatellier.clonewar.instruction;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface InstructionRepository extends ReactiveCrudRepository<Instruction, Long> {}
+@Repository
+public interface InstructionRepository extends CrudRepository<Instruction, Long> {}
