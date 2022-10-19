2a3373f0c5ee62339b528be7df494c5924cddb32
Sacane <johan.ramaroson@gmail.com>
2022-10-20 01:11:01 +0200

    add app config

---
+++ src/main/java/fr/ramatellier/clonewar/AppConfig.java
@@ -0,0 +1,12 @@
+package fr.ramatellier.clonewar;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@ComponentScan
+@EnableWebFlux
+public class AppConfig {
+
+}
