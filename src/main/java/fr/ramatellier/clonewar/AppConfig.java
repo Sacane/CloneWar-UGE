415d657f5b55a4113c5fec3b871a9ab46ae24bcf
Sacane <johan.ramaroson@gmail.com>
2022-10-20 17:52:06 +0200

    moved Artifact classes in one package

--- src/main/java/fr/ramatellier/clonewar/AppConfig.java
+++ src/main/java/fr/ramatellier/clonewar/AppConfig.java
@@ -1,12 +1,28 @@
 package fr.ramatellier.clonewar;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Configuration
 @ComponentScan
 @EnableWebFlux
 public class AppConfig {
+    @Bean
+    public TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager){
+        return new TransactionTemplate(transactionManager);
+    }
 
+    @Bean
+    public Scheduler schedulerCtx(){
+        return Schedulers.fromExecutor(Executors.newSingleThreadExecutor());
+    }
 }
---
+++ src/main/java/fr/ramatellier/clonewar/artifact/ArtifactService.java
@@ -0,0 +1,36 @@
+package fr.ramatellier.clonewar.artifact;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+
+@Service
+public class ArtifactService {
+
+    private final ArtifactRepository repository;
+    private final TransactionTemplate transactionTemplate;
+    private final Scheduler schedulerCtx;
+
+    public ArtifactService(ArtifactRepository repository, TransactionTemplate transactionTemplate, @Qualifier("schedulerCtx") Scheduler schedulerCtx){
+        this.repository = repository;
+        this.transactionTemplate = transactionTemplate;
+        this.schedulerCtx = schedulerCtx;
+    }
+
+    public Mono<ArtifactDTO> saveArtifact(Artifact entity){
+        var dto = new ArtifactDTO(entity.name(), entity.inputDate().toString(), entity.url());
+        return Mono.fromCallable(() -> transactionTemplate.execute(status -> {
+            repository.save(entity);
+            return dto;
+        })).subscribeOn(schedulerCtx);
+    }
+
+    public Flux<Artifact> findAll(){
+        var defer = Flux.defer(() -> Flux.fromIterable(repository.findAll()));
+        return defer.subscribeOn(schedulerCtx);
+    }
+}
