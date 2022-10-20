183af28c3d11d8f79bd16ce33d81c99766e62894
Sacane <johan.ramaroson@gmail.com>
2022-10-20 11:49:11 +0200

    remove useless imports

--- src/main/java/fr/ramatellier/clonewar/instruction/InstructionRepository.java
+++ src/main/java/fr/ramatellier/clonewar/instruction/InstructionRepository.java
@@ -1,7 +1,6 @@
 package fr.ramatellier.clonewar.instruction;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
