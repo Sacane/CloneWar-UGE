a58b271016c639a73ce58bf160ae03ec854ed13e
Sacane <johan.ramaroson@gmail.com>
2022-10-20 17:52:34 +0200

    Changed long Id type to UUID

--- src/main/java/fr/ramatellier/clonewar/instruction/InstructionRepository.java
+++ src/main/java/fr/ramatellier/clonewar/instruction/InstructionRepository.java
@@ -3,5 +3,7 @@ package fr.ramatellier.clonewar.instruction;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface InstructionRepository extends CrudRepository<Instruction, Long> {}
+public interface InstructionRepository extends CrudRepository<Instruction, UUID> {}
