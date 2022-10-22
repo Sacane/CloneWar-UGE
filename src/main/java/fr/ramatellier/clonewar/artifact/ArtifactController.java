bbf29eb035a372946477a84808158b9da837958b
Sacane <johan.ramaroson@gmail.com>
2022-10-23 01:27:52 +0200

    delete useless method in controller

--- src/main/java/fr/ramatellier/clonewar/artifact/ArtifactController.java
+++ src/main/java/fr/ramatellier/clonewar/artifact/ArtifactController.java
@@ -6,7 +6,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-import java.util.List;
+
 
 @RestController
 public class ArtifactController {
@@ -21,9 +21,5 @@ public class ArtifactController {
     @GetMapping(path = "/api/artifacts")
     public Flux<ArtifactDTO> retrieveAllArtifacts(){
         return service.findAll().delayElements(Duration.ofMillis(150)).map(m -> new ArtifactDTO(m.name(), m.url(), m.inputDate().toString()));
-    }
-    @GetMapping(path= "/api/test")
-    public Flux<String> send(){
-        return Flux.fromIterable(List.of("String1", "String2", "String3")).delayElements(Duration.ofMillis(150));
     }
 }
