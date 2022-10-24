1f05b09d2f76a26bce25bdca29990cae068f49eb
Sacane <johan.ramaroson@gmail.com>
2022-10-24 18:58:43 +0200

    undo artifactDTO implements DtoPersistable : An ArtfactDTO is not persistable, only for spec

--- src/main/java/fr/ramatellier/clonewar/artifact/ArtifactDTO.java
+++ src/main/java/fr/ramatellier/clonewar/artifact/ArtifactDTO.java
@@ -1,12 +1,2 @@
 package fr.ramatellier.clonewar.artifact;
-
-import fr.ramatellier.clonewar.DtoPersistable;
-
-import java.time.LocalDate;
-public record ArtifactDTO(String id, String name, String date, String url) implements DtoPersistable<Artifact> {
-
-    @Override
-    public Artifact toEntity() {
-        return new Artifact(name, url, LocalDate.parse(date));
-    }
-}
+public record ArtifactDTO(String id, String name, String date, String url) {}
