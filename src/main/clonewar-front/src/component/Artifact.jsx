31989ede9b1c0f804218f3b6c3b578038c8a3496
Sacane <johan.ramaroson@gmail.com>
2022-11-07 15:45:16 +0100

    [FRONT-Fix] - fixed data placement

--- src/main/clonewar-front/src/component/Artifact.jsx
+++ src/main/clonewar-front/src/component/Artifact.jsx
@@ -4,8 +4,8 @@ function Artifact({artifact:{id, name, date, url}}){
     return (
         <tr key={id}>
             <td>{name}</td>
-            <td>{date}</td>
             <td>{url}</td>
+            <td>{date}</td>
             <td><button className={"button is-rounded"}>detail</button></td>
         </tr>
     )
