acbb6d3a64ddffa2da03a3059d4123a4ba694287
Sacane <johan.ramaroson@gmail.com>
2022-11-09 18:02:02 +0100

    [FRONT-feat] - update app page, dispatch elements as flex parts

--- src/main/clonewar-front/src/App.jsx
+++ src/main/clonewar-front/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import "bulma/css/bulma.min.css";
 import Artifact from "./component/Artifact/Artifact.jsx";
-import Files from "./component/Files.jsx";
+import Upload from "./component/Upload/Upload.jsx";
 
 
 
@@ -11,8 +11,10 @@ function App() {
     <div className="App">
       <h1 className={"title is-1"}><b>Clonewar</b></h1>
       <p>Clonewar is an web-app that allow you to analyze jarfiles and find its clone among others projects</p>
-      <Files filename={"jar"}></Files>
-      <Artifact></Artifact>
+        <div className={"content"}>
+            <Upload></Upload>
+            <Artifact></Artifact>
+        </div>
     </div>
   )
 }
