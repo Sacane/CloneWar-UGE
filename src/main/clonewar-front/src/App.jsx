4b8008fd4baf7d8d5ae5d73bd415b1f060a9faee
Sacane <johan.ramaroson@gmail.com>
2022-11-12 18:53:03 +0100

    [FRONT - feat] Moves the indication to the first page

--- src/main/clonewar-front/src/App.jsx
+++ src/main/clonewar-front/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import "bulma/css/bulma.min.css";
 import Artifact from "./component/Artifact/Artifact.jsx";
 import Upload from "./component/Upload/Upload.jsx";
+import React from "react";
 
 
 
@@ -10,7 +11,8 @@ function App() {
   return (
     <div className="App">
       <h1 className={"title is-1"}><b>Clonewar</b></h1>
-      <p>Clonewar is an web-app that allow you to analyze jarfiles and find its clone among others projects</p>
+        <p>Clonewar is an web-app that allow you to analyze jarfiles and find its clone among others projects</p>
+        <p>Here is the list of the registered , click on the desired one to see its detail</p>
         <div className={"content"}>
             <Artifact></Artifact>
             <div className={"form"}>
