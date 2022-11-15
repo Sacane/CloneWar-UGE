a50a87b596860f26959ff52c4d2e9544ddd32b41
Sacane <johan.ramaroson@gmail.com>
2022-11-15 22:06:26 +0100

    [FRONT - FEAT] - readjust style of the main page

--- src/main/clonewar-front/src/App.jsx
+++ src/main/clonewar-front/src/App.jsx
@@ -10,9 +10,8 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1 className={"title is-1"}><b>Clonewar</b></h1>
+        <h1 className={"title is-1"}><b>Clonewar</b></h1>
         <p>Clonewar is an web-app that allow you to analyze jarfiles and find its clone among others projects</p>
-        <p>Here is the list of the registered , click on the desired one to see its detail</p>
         <div className={"content"}>
             <Artifact></Artifact>
             <div className={"form"}>
