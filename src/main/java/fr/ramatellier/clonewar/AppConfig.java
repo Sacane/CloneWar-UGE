2ab00f3814e46f8a179fb489b53391e243426bd5
Sacane <johan.ramaroson@gmail.com>
2022-11-20 01:03:31 +0100

    [GLOBAL] fixed integration of front as static index to jar

--- src/main/java/fr/ramatellier/clonewar/AppConfig.java
+++ src/main/java/fr/ramatellier/clonewar/AppConfig.java
@@ -1,15 +1,25 @@
 package fr.ramatellier.clonewar;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
+import javax.print.attribute.standard.Media;
 import java.util.concurrent.Executors;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Configuration
 @ComponentScan
@@ -23,5 +33,12 @@ public class AppConfig {
     @Bean
     public Scheduler schedulerCtx(){
         return Schedulers.fromExecutor(Executors.newSingleThreadExecutor());
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> htmlRouter(
+            @Value("classpath:/public/index.html") Resource html
+    ){
+        return route(GET("/"), request -> ok().contentType(MediaType.TEXT_HTML).syncBody(html));
     }
 }
