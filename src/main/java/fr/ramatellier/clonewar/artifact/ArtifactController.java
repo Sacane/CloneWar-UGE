9687e29ba8b196dc7e50cd8aca71c2ebad3d40a7
Sacane <johan.ramaroson@gmail.com>
2022-11-21 20:16:11 +0100

    [BACK - FIX] - fix methods adding an artifact only with a jar archive

--- src/main/java/fr/ramatellier/clonewar/artifact/ArtifactController.java
+++ src/main/java/fr/ramatellier/clonewar/artifact/ArtifactController.java
@@ -34,25 +34,13 @@ public class ArtifactController {
     @GetMapping(path = "/api/artifacts")
     public Flux<ArtifactDTO> retrieveAllArtifacts(){
         LOGGER.info("Starting to retrieve all artifacts in database");
-        return service.findAll().delayElements(Duration.ofMillis(150)).map(Artifact::toDto);
-    }
-
-    public Mono<Void> saveArtifact(ArtifactUploadDTO dto){
-        return service.saveArtifact(dto);
+        return service.findAll().map(Artifact::toDto);
     }
 
     @PostMapping(path="/api/artifact/upload", headers = "content-type=multipart/*")
-    public Mono<Void> uploadJarFile(@RequestPart("jar") Mono<FilePart> jarFile){
+    public Mono<ArtifactDTO> uploadJarFile(@RequestPart("jar") FilePart jarFile){
         LOGGER.info("Attempt to upload a file: ");
-        return jarFile
-                .doOnNext(fp -> LOGGER.info("Received file : " + fp.filename()))
-                .flatMap(fp -> fp.transferTo(UPLOAD_PATH.resolve(fp.filename())))
-                .then();
+        return service.createArtifactFromFileAndThenPersist(jarFile);
     }
 
-    @PostMapping(path = "/api/artifact/create")
-    public Mono<ArtifactSaveDTO> buildAndPersistArtifact(@RequestBody ArtifactSaveDTO dto) throws IOException {
-        LOGGER.info("Attempt to create an artifact : ");
-        return service.saveArtifactWithInstruction(dto);
-    }
 }
--- src/main/java/fr/ramatellier/clonewar/artifact/ArtifactService.java
+++ src/main/java/fr/ramatellier/clonewar/artifact/ArtifactService.java
@@ -2,9 +2,9 @@ package fr.ramatellier.clonewar.artifact;
 
 import fr.ramatellier.clonewar.artifact.dto.ArtifactDTO;
 import fr.ramatellier.clonewar.artifact.dto.ArtifactSaveDTO;
-import fr.ramatellier.clonewar.artifact.dto.ArtifactUploadDTO;
 import fr.ramatellier.clonewar.instruction.InstructionBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
 import reactor.core.publisher.Flux;
@@ -12,9 +12,7 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.time.LocalDate;
 import java.util.logging.Logger;
 
@@ -42,39 +40,23 @@ public class ArtifactService {
     public Mono<ArtifactDTO> saveArtifact(Artifact entity){
         return Mono.fromCallable(() -> transactionTemplate.execute(status -> {
             var entityResponse = repository.save(entity);
-            return new ArtifactDTO(entityResponse.id().toString(), entityResponse.name(), entityResponse.inputDate().toString(), entityResponse.url());
+            return new ArtifactDTO(entityResponse.id().toString(), entityResponse.name(), entityResponse.inputDate().toString(), entityResponse.url(), null);
         })).subscribeOn(schedulerCtx);
-    }
-    public Mono<Void> saveArtifact(ArtifactUploadDTO dto){
-        var file = new File(dto.name());
-        return dto.document().transferTo(file)
-                .publishOn(Schedulers.boundedElastic())
-                .doOnNext(p -> {
-                    try {
-                        var entity = new Artifact(dto.name(), dto.url(), LocalDate.parse(dto.date()),
-                                Files.readAllBytes(file.toPath()));
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
     }
 
-    /**
-     * TODO Replace
-     * @param dto
-     * @return
-     * @throws IOException
-     */
-    public Mono<ArtifactSaveDTO> saveArtifactWithInstruction(ArtifactSaveDTO dto) throws IOException {
-        LOGGER.info("Parsing artifacts and its instructions");
-        var artifact = new Artifact(dto.name(), dto.url(), LocalDate.now());
-        var list = InstructionBuilder.buildInstructionFromJar(ArtifactController.UPLOAD_PATH.resolve(dto.url()).toString());
-        return Mono.fromCallable(() -> transactionTemplate.execute(status -> {
-            artifact.addAllInstructions(list);
-            var entityResponse = repository.save(artifact);
-            LOGGER.info("Saving artifact done.");
-            return new ArtifactSaveDTO(entityResponse.name(), entityResponse.inputDate().toString(), entityResponse.url());
-        })).subscribeOn(schedulerCtx);
+    //TODO remove at end
+    public Mono<ArtifactDTO> createArtifactFromFileAndThenPersist(FilePart filePart){
+        LOGGER.info("Start parsing a file");
+        LOGGER.info("On schedule file :" + filePart.filename());
+        return filePart.content().publishOn(Schedulers.boundedElastic()).map(signal -> {
+            try(var sig = signal.asInputStream()){
+                var bytes = sig.readAllBytes();
+                var artifact = new Artifact(filePart.filename(), filePart.filename(), LocalDate.now(), bytes);
+                return repository.save(artifact).toDto();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).single();
     }
 
     public Flux<Artifact> findAll(){
