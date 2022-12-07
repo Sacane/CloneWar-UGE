b97ca0217e7fd4cc646d939ec0426258244f9b19
Sacane <johan.ramaroson@gmail.com>
2022-12-07 01:03:10 +0100

    [BACK - fix] - reformat file

--- src/main/java/fr/ramatellier/clonewar/artifact/ArtifactService.java
+++ src/main/java/fr/ramatellier/clonewar/artifact/ArtifactService.java
@@ -1,26 +1,19 @@
 package fr.ramatellier.clonewar.artifact;
 
-import com.mchange.util.AssertException;
 import fr.ramatellier.clonewar.artifact.dto.ArtifactDTO;
+import fr.ramatellier.clonewar.exception.InvalidJarException;
 import fr.ramatellier.clonewar.instruction.InstructionBuilder;
-import fr.ramatellier.clonewar.util.AsmParser;
-import fr.ramatellier.clonewar.util.ByteResourceReader;
 import fr.ramatellier.clonewar.util.PomExtractor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.support.TransactionTemplate;
-import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.logging.Logger;
 
@@ -29,27 +22,11 @@ public class ArtifactService {
 
     private final static Logger LOGGER = Logger.getLogger(ArtifactService.class.getName());
     private final ArtifactRepository repository;
-    private final TransactionTemplate transactionTemplate;
     private final Scheduler schedulerCtx;
-    private static final String UPLOAD_PATH = "./src/main/resources/upload/";
 
-    public ArtifactService(ArtifactRepository repository, TransactionTemplate transactionTemplate, @Qualifier("schedulerCtx") Scheduler schedulerCtx){
+    public ArtifactService(ArtifactRepository repository, @Qualifier("schedulerCtx") Scheduler schedulerCtx){
         this.repository = repository;
-        this.transactionTemplate = transactionTemplate;
         this.schedulerCtx = schedulerCtx;
-    }
-
-    /**
-     * TODO REMOVE
-     * @Deprecated
-     * @param entity
-     * @return
-     */
-    public Mono<ArtifactDTO> saveArtifact(Artifact entity){
-        return Mono.fromCallable(() -> transactionTemplate.execute(status -> {
-            var entityResponse = repository.save(entity);
-            return new ArtifactDTO(entityResponse.id().toString(), entityResponse.name(), entityResponse.inputDate().toString(), entityResponse.url(), null);
-        })).subscribeOn(schedulerCtx);
     }
 
     //SRC -> .JAVA
@@ -65,25 +42,32 @@ public class ArtifactService {
         return artifact;
     }
 
-    public Mono<ArtifactDTO> createArtifactFromFileAndThenPersist(FilePart filePart){
-        LOGGER.info("Persist file :" + filePart.filename());
-        return filePart.content().publishOn(Schedulers.boundedElastic()).map(signal -> {
-            try(var sig = signal.asInputStream()){
-                var bytes = sig.readAllBytes();
-                var srcBytes = Files.readAllBytes(Path.of("src/test/resources/samples/SeqSrc.jar")); //TODO remove this and replace by the given src jar(from front)
-                var artifact = createArtifactByInfos(srcBytes, bytes);
-                return repository.save(artifact).toDto();
+    private Mono<byte[]> retrieveBytesFromFilePart(FilePart filePart){
+        return filePart.content().publishOn(Schedulers.boundedElastic()).map(part -> {
+            try {
+                return part.asInputStream().readAllBytes();
             } catch (IOException e) {
-                throw new AssertionError(e);
+                throw new InvalidJarException(e);
             }
         }).single();
+    }
+
+    public Mono<ArtifactDTO> createArtifactFromFileAndThenPersist(FilePart mainPart, FilePart srcPart){
+        LOGGER.info("Persist files main :" + mainPart.filename() + " and its src : " + srcPart.filename());
+        return retrieveBytesFromFilePart(mainPart)
+                .flatMap(mainBytes -> retrieveBytesFromFilePart(srcPart).map(srcBytes -> {
+                                    try {
+                                        var artifact = createArtifactByInfos(srcBytes, mainBytes);
+                                        return repository.save(artifact).toDto();
+                                    } catch (IOException e) {
+                                        throw new InvalidJarException(e);
+                                    }
+                                }
+                ));
     }
 
     public Flux<Artifact> findAll(){
         var defer = Flux.defer(() -> Flux.fromIterable(repository.findAll()));
         return defer.subscribeOn(schedulerCtx);
-    }
-    public void configure(){
-        var artifact = repository.findByName("");
     }
 }
