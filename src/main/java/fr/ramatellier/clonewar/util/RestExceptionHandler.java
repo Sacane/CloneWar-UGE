d8bdf1460a1ffad049e10051d9633972611aad0c
Sacane <johan.ramaroson@gmail.com>
2022-12-07 01:02:08 +0100

    [BACK - feat] - set exception when giving a unexpected jar content

--- src/main/java/fr/ramatellier/clonewar/util/RestExceptionHandler.java
+++ src/main/java/fr/ramatellier/clonewar/util/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package fr.ramatellier.clonewar.util;
 
+import fr.ramatellier.clonewar.exception.InvalidJarException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,5 +13,10 @@ public class RestExceptionHandler {
     @ExceptionHandler(IOException.class)
     ResponseEntity<Void> fileNotFound(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(InvalidJarException.class)
+    ResponseEntity<Void> badJarInjected(){
+        return ResponseEntity.badRequest().build();
     }
 }
