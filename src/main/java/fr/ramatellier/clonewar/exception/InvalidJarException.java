7217083f0e8a37bb9bf3e8f33ee3850c130031fd
Sacane <johan.ramaroson@gmail.com>
2022-12-07 01:02:08 +0100

    [BACK - feat] - set exception when giving a unexpected jar content

---
+++ src/main/java/fr/ramatellier/clonewar/exception/InvalidJarException.java
@@ -0,0 +1,17 @@
+package fr.ramatellier.clonewar.exception;
+
+public class InvalidJarException extends RuntimeException{
+
+    private static final String MESSAGE = "The given jar is not valid";
+
+    public InvalidJarException(Throwable t){
+        super(t);
+    }
+    public InvalidJarException(){
+        super(MESSAGE);
+    }
+    public InvalidJarException(String message, Throwable t){
+        super(MESSAGE, t);
+    }
+
+}
