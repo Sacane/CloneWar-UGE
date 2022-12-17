1be9a2141fd6f8aa5cee5913ca5a945476387e5e
qtdrake <quentintellier01@yahoo.com>
2022-12-17 15:55:30 +0100

    [(Back/Front) (Feat) Change to hash instructions like rabin karp]

--- src/main/java/fr/ramatellier/clonewar/util/Kaplin.java
+++ src/main/java/fr/ramatellier/clonewar/util/Kaplin.java
@@ -2,25 +2,9 @@ package fr.ramatellier.clonewar.util;
 
 import fr.ramatellier.clonewar.instruction.Instruction;
 
-import java.io.IOException;
 import java.util.List;
 
 public class Kaplin {
-    private static boolean rabinKarp(String s, String pattern) {
-        var hashPattern = Hasher.hash(pattern);
-
-        for(var i = 0; i < s.length() - pattern.length() + 1; i++) {
-            var subS = s.substring(i, i + pattern.length() - 1);
-            System.out.println(subS);
-            var hashS = Hasher.hash(subS);
-            if(hashS == hashPattern) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     public static boolean compareInstructionWithJarInstructions(Instruction instruction, List<Instruction> instructions) {
         for(var elem: instructions) {
             if(elem.hashValue() == instruction.hashValue() && elem.content().equals(instruction.content())) {
@@ -31,7 +15,7 @@ public class Kaplin {
         return false;
     }
 
-    public static int compareJarInstructions(List<Instruction> instructionsJar1, List<Instruction> instructionsJar2) throws IOException {
+    public static int compareJarInstructions(List<Instruction> instructionsJar1, List<Instruction> instructionsJar2) {
         var nbActualInstruction = 0;
         var nbInstruction = instructionsJar1.size();
 
