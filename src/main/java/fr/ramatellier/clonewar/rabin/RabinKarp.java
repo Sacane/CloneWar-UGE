febd4a34b6ea9ea9ce2689a92b4ef5c9c7902c1c
qtdrake <quentintellier01@yahoo.com>
2022-12-22 21:48:07 +0100

    [(Back) (Feat) Modifications to get the score]

--- src/main/java/fr/ramatellier/clonewar/rabin/RabinKarp.java
+++ src/main/java/fr/ramatellier/clonewar/rabin/RabinKarp.java
@@ -2,6 +2,7 @@ package fr.ramatellier.clonewar.rabin;
 
 import fr.ramatellier.clonewar.instruction.Instruction;
 
+import java.util.Comparator;
 import java.util.List;
 
 public final class RabinKarp {
@@ -19,11 +20,35 @@ public final class RabinKarp {
         var nbActualInstruction = 0;
         double nbInstruction = instructionsJar1.size();
 
-        for(var instruction: instructionsJar1) {
+        instructionsJar1 = instructionsJar1.stream().sorted(Comparator.comparing(Instruction::hashValue)).toList();
+        instructionsJar2 = instructionsJar2.stream().sorted(Comparator.comparing(Instruction::hashValue)).toList();
+        var i = 0;
+        var j = 0;
+
+        while(i < nbInstruction) {
+            var instruction1 = instructionsJar1.get(i);
+            var instruction2 = instructionsJar2.get(j);
+
+            if(instruction1.hashValue() < instruction2.hashValue()) {
+                i++;
+            }
+            else if(instruction1.hashValue() > instruction2.hashValue()) {
+                if(j == instructionsJar2.size() - 1)
+                    break;
+                j++;
+            }
+            else {
+                if(instruction1.content().equals(instruction2.content()))
+                    nbActualInstruction++;
+                i++;
+            }
+        }
+
+        /*for(var instruction: instructionsJar1) {
             if(compareInstructionWithJarInstructions(instruction, instructionsJar2)) {
                 nbActualInstruction++;
             }
-        }
+        }*/
 
         return (int) ((nbActualInstruction / nbInstruction) * 100);
     }
