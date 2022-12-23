7d9f47a059ff8304a274aca79ab26ea65ab378cd
Sacane <johan.ramaroson@gmail.com>
2022-12-23 19:23:00 +0400

    [FRONT - FIX] - remove useless instruction

--- src/main/clonewar-front/src/pages/Detail/Detail.jsx
+++ src/main/clonewar-front/src/pages/Detail/Detail.jsx
@@ -37,15 +37,14 @@ function Detail(props) {
     return (
         <div className="Detail">
             <h1 className={"title is-1"}>Detail for artifact: { mainName }</h1>
-
-            <p>This page will show you the score of the selected artifact with all the others artifact that have been added to the CloneWar application.</p>
-
             <div className={"table-wrapper"}>
-                <p><b>Here is the list of the registered, click on the desired one to see its detail</b></p>
-                <p><b>There is three differents cases for the score:</b></p>
-                <p><b>Between 0-50 it's not a clone</b></p>
-                <p><b>Between 50-80 it can be a clone</b></p>
-                <p><b>Between 80-100 it's a clone</b></p>
+                <div className={"indication"}>
+                    <p><b>Here is the list of the registered, click on the desired one to see its detail</b></p>
+                    <p><b>There is three differents cases for the score:</b></p>
+                    <p><b>Between 0-50 it's not a clone</b></p>
+                    <p><b>Between 50-80 it can be a clone</b></p>
+                    <p><b>Between 80-100 it's a clone</b></p>
+                </div>
                 <table id={"artifacts"} className={"table is-fullwidth"}>
                     <thead>
                         <tr>
