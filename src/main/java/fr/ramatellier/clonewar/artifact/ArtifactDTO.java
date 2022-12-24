ee824e0df7f0af413f6ed0ea476d59b6303acecb
Sacane <johan.ramaroson@gmail.com>
2022-12-24 13:15:50 +0400

    [BACK - BACK] - add version for artifact

--- src/main/java/fr/ramatellier/clonewar/artifact/ArtifactDTO.java
+++ src/main/java/fr/ramatellier/clonewar/artifact/ArtifactDTO.java
@@ -1,2 +1,2 @@
 package fr.ramatellier.clonewar.artifact;
-public record ArtifactDTO(String id, String name, String date, String url, String urlSrc) {}
+public record ArtifactDTO(String id, String name, String date, String url, String urlSrc, String version) {}
